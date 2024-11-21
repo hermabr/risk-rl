274e1bfd3943e31d100fd4240f1f173efb4d5940
AndriFV <afv6@hi.is>
2024-11-20 21:26:43 -0800

    test decode attack option

--- risk/__main__.py
+++ risk/__main__.py
@@ -7,14 +7,20 @@ import traceback
 
 logging_setup.init_logging()
 
-
 players = [PlayerHeuristic("Player 1"), PlayerHeuristic("Player 2"),
             PlayerHeuristic("Player 3"), PlayerHeuristic("Player 4"),
             PlayerHeuristic("Player 5")]
 
 game = Game(players, display_map=True, delay=False) 
 
+
+def test_decode_attack_options(game: Game):
+    for attack_options in range(game.total_attack_options_cnt):
+        attack_country, defend_country, n_soldiers = game.decode_attack_option(attack_options)
+        print(attack_country, defend_country, n_soldiers)
+
 try:
+    test_decode_attack_options(game)
     game.gameplay_loop()
 except Exception as e:
     logging.error("An error occurred: %s", str(e))
